01a200e1c11ab61345517df39a51fea857161488
Ivan Villacorta <ivan_mv23@hotmail.com>
2022-08-31 20:28:21 -0300

    feat(unit-test): unit test component header

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,22 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import * as Feather from 'feather-icons';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
+  // 
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [HeaderComponent]
     })
-    .compileComponents();
+      .compileComponents();
+
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+    spyOn(Feather, 'replace');
+  });
+
+  it('should import icon', () => {
+
+    component.ngAfterViewInit();
+    expect(Feather.replace).toHaveBeenCalled();
   });
 
 });
